81029262efa6d8e0a27369bb76af1f68df9d877c
SaiC <sjc65@njit.edu>
2023-08-07 21:03:11 -0400

    attempted highlight feature (doesn't work)

--- Project/client/views/UserListPanel.java
+++ Project/client/views/UserListPanel.java
@@ -21,7 +21,9 @@ import Project.client.ICardControls;
 public class UserListPanel extends JPanel {
     JPanel userListArea;
     private static Logger logger = Logger.getLogger(UserListPanel.class.getName());
-
+    // -------------------------------------
+    private long senderId;
+    // -------------------------------------
     public UserListPanel(ICardControls controls) {
         super(new BorderLayout(10, 10));
         JPanel wrapper = new JPanel();
@@ -67,7 +69,7 @@ public class UserListPanel extends JPanel {
         logger.log(Level.INFO, "Adding user to list: " + clientName);
         JPanel content = userListArea;
         logger.log(Level.INFO, "Userlist: " + content.getSize());
-        JEditorPane textContainer = new JEditorPane("text/plain", clientName);
+        JEditorPane textContainer = new JEditorPane("text/html", textFormat(clientName));   // Reroutes clientName through the method which returns the new format
         textContainer.setName(clientId + "");
         // sizes the panel to attempt to take up the width of the container
         // and expand in height based on word wrapping
@@ -100,4 +102,40 @@ public class UserListPanel extends JPanel {
             userListArea.remove(c);
         }
     }
+//----------------------------------------------------------------------------------
+/*
+ *  UCID: sjc65
+ *  Date: 08/07/2023
+ *  Explanation: The code block shows three methods. The first method retrieves and sets clientId from the ClientUI, the second method
+ *  gets the clientId and the third method handles the main text formatting. The textFormat method takes in a clientName and
+ *  assigns it to a new variable called formattedClientName. The for-loop iterates over the components in the userListArea
+ *  and the if-statement checks if the client name is equal to the clientId from the ClientUI. If it is, then the font
+ *  color is changed to blue, if it doesn't then the font color is changed to black. The formatText() method replaces the
+ *  "clientName" in the JEditorPane textContainer = new JEditorPane("text/html", textFormat(clientName)); in addUserListItem() method.
+ */
+    public void setSenderID(long clientId) {
+        this.senderId = clientId;
+    }
+
+    public long getSenderID() {
+        return senderId;
+    }
+
+     public String textFormat(String clientName) {
+        String formattedClientName = clientName;
+        Component[] cs = userListArea.getComponents();
+        for(Component c : cs) {
+            if(c.getName().equals(getSenderID() + "")) {
+                formattedClientName = "<font color=\"blue\">" + clientName + "</font>";
+                //return formattedClientName;
+                break;
+            } else {
+                formattedClientName = "<font color=\"black\">" + clientName + "</font>";
+                //return formattedClientName;
+                break;
+            }
+        }
+        return formattedClientName;
+    }
+//-----------------------------------------------------------------------------------
 }
